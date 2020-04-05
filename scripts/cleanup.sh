714d693ab237343fa130355dd1fc03bc642f97e5
Zdenek Vydra <xvydra@fi.muni.cz>
2020-04-05 18:04:06 +0200

    first version

---
+++ scripts/cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+# disable root login using password
+sudo passwd -l root
+
+# cleanup
+apt-get -y autoremove
+apt-get -y autoclean
+rm -rf /var/log/*
+history -c
---
+++ scripts/guestAdditions.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -x
+
+ISOPATH="$HOME/VBoxGuestAdditions.iso"
+
+if [ ! -e "$ISOPATH" ]; then
+    echo "Error: virtualbox guest additions ISO not found at $ISOPATH"
+    exit 1
+fi
+
+sudo apt-get install build-essential dkms linux-headers-$(uname -r) -y
+
+# Mount virtualbox guest additions ISO and install
+sudo mkdir -p /tmp/VBoxGuestAdditions
+sudo mount -o loop,ro "$ISOPATH" /tmp/VBoxGuestAdditions
+echo "yes" | sudo /tmp/VBoxGuestAdditions/VBoxLinuxAdditions.run --nox11
+
+sudo rm "$ISOPATH"
+sudo umount /tmp/VBoxGuestAdditions
+sudo rmdir /tmp/VBoxGuestAdditions
