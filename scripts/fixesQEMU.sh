d7b74cfe9ea4a193b328b1a7234443def4eb0fba
Zdenek Vydra <xvydra@fi.muni.cz>
2020-08-03 10:34:56 +0200

    Update iso_url, remove unnecessary fix for QEMU

--- scripts/fixesQEMU.sh
+++ scripts/fixesQEMU.sh
@@ -1,9 +1,2 @@
 #!/bin/sh -x
 
-# disable qxl
-lsmod | grep "^qxl" > /dev/null
-if [ $? -eq 0 ] ; then
-    sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=\"\(.*\)\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\1 qxl.modeset=0\"/g' /etc/default/grub
-    sudo update-grub
-fi
-
